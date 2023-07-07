aa60cf6318137151fae0f194974bf2d34cc599f4
sayed.ahmed <sayed.ahmed@nextory.com>
2023-07-07 12:40:02 +0530

    Initial Commit

---
+++ src/test/java/com/pipeline/sample/SampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pipeline.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
